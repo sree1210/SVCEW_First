2751d1f5d2f8a0b53e6ee836879df6b2e468b8f1
Tsuser <Tsuser@10.0.43.27>
2019-05-10 10:21:41 +0530

    Second Upload Adding Two More Java Files Demo2 and Demo3.

---
+++ GitDemoProject/src/Demo2.java
@@ -0,0 +1,6 @@
+
+public class Demo2 {
+	public String secondMessage() {
+		return "My Second Message";
+	}
+}
---
+++ GitDemoProject/src/Demo3.java
@@ -0,0 +1,12 @@
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		Demo1 demo1 = new Demo1();
+		Demo2 demo2 = new Demo2();
+		
+		System.out.println("Demo1 : " + demo1.firstMessage());
+		System.out.println("Demo2 : " + demo2.secondMessage());
+	}
+
+}
